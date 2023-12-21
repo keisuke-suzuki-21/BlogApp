266e6aba3d385bbc6bb05baba4f1bc5427d09a9f
keisukesuzuki <keisuke0121tennis6060@ezweb.ne.jp>
2023-12-21 11:02:55 +0900

    articlesStore.jsのdefineStoreの定義方法変更

--- src/stores/articlesStore.js
+++ src/stores/articlesStore.js
@@ -1,32 +1,32 @@
 import { defineStore } from "pinia";
 
-// export const useArticleStore = defineStore("articleStore", {
-//     state: () => {
-//         return {
-//             count: 0
-//         }
-//     },
-//     getters: {
-//         double: (state) => {
-//             return state.count * 2
-//         }
-//     },
-//     actions:{
-//         increment() {
-//             this.count++
-//         }
-//     },
-//     persist: true
-// })
-
 export const useArticleStore = defineStore("articleStore", {
-    const count = ref(0);
-    const doubleCount = computed(() => count.value * 2);
-	const increment = () => {
-		console.log('increment');
-		count.value += 1;
-		console.log(count.value);
-	};
+    state: () => {
+        return {
+            count: 0
+        }
+    },
+    getters: {
+        double: (state) => {
+            return state.count * 2
+        }
+    },
+    actions:{
+        increment() {
+            this.count++
+        }
+    },
     persist: true
-    return { count, doubleCount, increment };
 })
+
+// export const useArticleStore = defineStore("articleStore", {
+//     const count = ref(0);
+//     const doubleCount = computed(() => count.value * 2);
+// 	const increment = () => {
+// 		console.log('increment');
+// 		count.value += 1;
+// 		console.log(count.value);
+// 	};
+//     persist: true
+//     return { count, doubleCount, increment };
+// })
